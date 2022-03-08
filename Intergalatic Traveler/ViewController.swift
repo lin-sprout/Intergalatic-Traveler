b3a25e2de336ca28d602adf3d7a35aaf591d85b7
Lin LaMonte <penelopelamonte@gmail.com>
2022-03-08 14:12:09 -0700

    Bridged Buttons

--- Intergalatic Traveler/ViewController.swift
+++ Intergalatic Traveler/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignbackground()
     }
-
+    func assignbackground()
+    {
+        let background = UIImage(named: "Space-bg")
+        var imageView : UIImageView
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+        
+    }
+    
+    @IBAction func redDwarfVisitButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func blueDwarfVisitButton(_ sender: Any)
+    {
+    
+    }
+    
+    
 
 }
 
